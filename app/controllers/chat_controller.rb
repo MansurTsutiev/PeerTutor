7301a42fe070169e5f20e14a5bce7b2af9277b64
MansurTsutiev <nyse_tm@yahoo.com>
2018-01-10 21:03:20 -0500

    comment out the chat_controller

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,5 +1,6 @@
 class ChatController < ApplicationController
   def index
+    #if conversations array doesn't exist, set conversations and point it to new array
     session[:conversations] ||= []
 
 
@@ -9,6 +10,8 @@ class ChatController < ApplicationController
     if !tutoring_session.nil? && tutoring_session.accepted
       @conversations = []
       @users = User.all.where(id: tutoring_session.user_id).or(User.all.where(id: tutoring_session.tutor_id)).where.not(id: current_user)                 #select all users that are not us
+      #Includes method eager loads all the users and the associated messages
+      #joins these 3 tables and returns rows with Conversation id specified
       @conversations << Conversation.includes(:recipient, :messages)
                                    .find_by(id: session[:conversations])
     end
