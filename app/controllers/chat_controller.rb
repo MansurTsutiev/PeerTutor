705c30e361e87f00f9b55b72433a6fe36e4e4476
MansurTsutiev <nyse_tm@yahoo.com>
2018-01-11 13:11:40 -0500

    refactor: filter out users for tutee side and tutor side

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -4,16 +4,28 @@ class ChatController < ApplicationController
     session[:conversations] ||= []
 
 
-    tutoring_session = TutoringSession.where(user_id: current_user.id).or(TutoringSession.where(tutor_id: current_user.id)).last
+    tutoring_sessions = TutoringSession.where(user_id: current_user.id).or(TutoringSession.where(tutor_id: current_user.id))
 
 
-    if !tutoring_session.nil? && tutoring_session.accepted
+    if tutoring_sessions.count > 1
+      #tutor
       @conversations = []
-      @users = User.all.where(id: tutoring_session.user_id).or(User.all.where(id: tutoring_session.tutor_id)).where.not(id: current_user)                 #select all users that are not us
-      #Includes method eager loads all the users and the associated messages
-      #joins these 3 tables and returns rows with Conversation id specified
-      @conversations << Conversation.includes(:recipient, :messages)
-                                   .find_by(id: session[:conversations])
+
+      user_ids = TutoringSession.where(tutor_id: current_user.id).pluck(:user_id)
+      @users = User.find(user_ids)
+
+      @conversations << Conversation.includes(:recipient, :messages).find_by(id: session[:conversations])
+    elsif tutoring_sessions.count == 1
+      #tutee
+      tutoring_session = tutoring_sessions[0]
+      if !tutoring_session.nil? && tutoring_session.accepted
+        @conversations = []
+        @users = User.all.where(id: tutoring_session.user_id).or(User.all.where(id: tutoring_session.tutor_id)).where.not(id: current_user)                 #select all users that are not us
+        #Includes method eager loads all the users and the associated messages
+        #joins these 3 tables and returns rows with Conversation ids (array of id's) specified
+        @conversations << Conversation.includes(:recipient, :messages)
+                                     .find_by(id: session[:conversations])
+      end
     end
-  end
-end
+  end #end index
+end #end class
